7a2f9d92b915c75b80a6ef50e32b6f29f08d64a6
Praneeth molleti <43575100+praneethmolleti@users.noreply.github.com>
2020-07-19 00:24:44 +0530

    Add files via upload

---
+++ dailyroutine.py
@@ -0,0 +1,71 @@
+#Daily routine guide
+ans=["yes","Yes","Y","y","yup","Aa","Yup","aa"]
+z=input("Are you at Home")
+if z in ans:
+    print("Alarm Rings")
+    print("Brush your teeth")
+    print("Have Breakfast")
+    x=input("is there any netacad exam today?")
+    y=input("is there any coursera submission today?")
+    if x in ans:
+        print("shedule an alarm for 4 in the evening")
+    if y in ans:
+        print("Try getting messed up with the assignment today-deadline blinking up")
+    else:
+        print("do watch your favourite movie for 4 hours")
+
+    print("get breakfast at 9")
+    print("work for 4 hours")
+    print("get lunch")
+    print("return to work")
+    if x in ans:
+        print("open netacad exam portal at 3:59")
+        print("write the exam till 5'o' clock")
+    print("get out of the work at 5 to play badminton")
+    print("take 15 min rest at 6")
+    print("take bath")
+    print("get dinner arrangements done at 6:45")
+    print("have dinner at 7")
+    print("spend time with family till 8")
+    print("return to work at 8")
+    print("work till 11")
+    print("get the badam sink in water for 10 hours")
+    print("Go and sleep you #$@%^!")
+else:
+    print("wake up at 8:45-desclaimer:this is not a home")
+    print("refresh in 15 min")
+    print("take breakfast they serve you if you call it so")
+    print("get into the classromm at 9:29:59")
+    a=input("is the class interesting")
+    if a in ans:
+        print("go and listen concentrate-It's an Engineering college Obviously")
+    else:
+        b=print("see whether your girlfriend is paying attention to the class")
+        if b in ans:
+            print("Open facebook,instagram followed by twitter")
+        else:
+            print("keep texting your girlfriend till 12:45-because we always should satisfy them")
+    print("class gets completed at 12:45")
+    print("get to the mess to have lunch at 1")
+    print("Arey Praneeth!Eat fast Let's play chess in the dorm")
+    c=input("is the chess game completed at 1:45")
+    if c in ans:
+        print("let's get face wash- because we know the primary purpose of going to college")
+    else:
+        print("Bunk the class - chess is more lovable and addictable than your girlfriend")
+        print("play till 5")
+    print("It's 5 buddy")
+    print("let's get into the badminton court fast")
+    d=input("is the court empty")
+    if d in ans:
+        print("play till 6:45")
+    else:
+        print("go to walk and return by 6:45")
+    print("Mess bell rings at 7")
+    print("go to mess and get time pass till 8")
+    print("call your parents and then open your favourite web series")
+    e=input("did your girlfriend called you or texted you?")
+    if e in ans:
+        print("It's night out again.Oh God! Help me")
+    else:
+        print("Hurray!Sleep tight till 8:45")
